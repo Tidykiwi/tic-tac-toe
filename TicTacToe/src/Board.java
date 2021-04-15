3a46e5fe720c0c07d6ac9d8e82e4924e306b2f65
Tidykiwi <rosskeithbaker@gmail.com>
2021-04-15 20:26:54 +1200

    Minor spelling changes to Board class variables

--- TicTacToe/src/Board.java
+++ TicTacToe/src/Board.java
@@ -25,7 +25,7 @@ public class Board {
 		
 
 		 /** Return true if it is a draw (i.e., no more EMPTY cells) */ 
-		//public boolean isDraw() {
+		public boolean isDraw() {
 			 
 			// TODO: Check whether the game has ended in a draw. 
 			// Hint: Use a nested loop (see the constructor for an example). Check whether any of the cells content in the board grid are Player.Empty. If they are, it is not a draw.
@@ -34,7 +34,7 @@ public class Board {
 			
 
 			
-		//}
+		}
 		
 		/** Return true if the current player "thePlayer" has won after making their move  */
 		public boolean hasWon(Player thePlayer, int playerRow, int playerCol) {
